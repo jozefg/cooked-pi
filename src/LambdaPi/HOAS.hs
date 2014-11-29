8e7cb56a5acb2a0fa860545f60513ee194af2368
Danny Gratzer <jozefg@cmu.edu>
2014-11-29 13:09:53 -0500

    First pass at implementing normalization

--- src/LambdaPi/HOAS.hs
+++ src/LambdaPi/HOAS.hs
@@ -9,7 +9,7 @@ data IExpr = App IExpr CExpr
            | Bool
            | Star
            | Pi CExpr (CExpr -> CExpr)
-           | Const String
+           | C String
            | IGen Int
 
 data CExpr = Lam (CExpr -> CExpr)
@@ -19,7 +19,17 @@ type NFI = IExpr
 type NFC = CExpr
 
 inf :: IExpr -> NFI
-inf = undefined
+inf ETrue = ETrue
+inf EFalse = EFalse
+inf Bool = Bool
+inf Star = Star
+inf (C s) = C s
+inf (IGen i) = IGen i
+inf (Annot l r) = Annot (cnf l) (cnf r)
+inf (Pi t f) = Pi (cnf t) (cnf . f)
+inf (App l r) = case inf l of
+  (Annot (Lam f) (CI (Pi _ g))) -> Annot (cnf . f $ CI l) (cnf . g $ CI l)
+  l' -> App l' (cnf r)
 
 cnf :: CExpr -> NFC
 cnf (CI e) = CI (inf e)
@@ -40,6 +50,8 @@ ieqTerm Star Star = return True
 ieqTerm Bool Bool = return True
 ieqTerm ETrue ETrue = return True
 ieqTerm EFalse EFalse = return True
+ieqTerm (Annot l r) (Annot l' r') = (&&) <$> ceqTerm l l' <*> ceqTerm r r'
+ieqTerm (App l r) (App l' r') = (&&) <$> ieqTerm l l' <*> ceqTerm r r'
 ieqTerm (Pi t f) (Pi t' g) =
   (&&) <$> ceqTerm t t' <*> (gen >>= \v -> ceqTerm (f $ CI v) (g $ CI v))
 ieqTerm (IGen i) (IGen j) = return (i == j)
