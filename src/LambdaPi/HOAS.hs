0aec07c86f26d5cf0215bb0dbd3169e56f55f568
Danny Gratzer <jozefg@cmu.edu>
2014-11-29 13:34:03 -0500

    Removed distinction between infer and check

--- src/LambdaPi/HOAS.hs
+++ src/LambdaPi/HOAS.hs
@@ -1,58 +1,65 @@
 module LambdaPi.HOAS where
-import Control.Applicative
-import Control.Monad.Gen
+import           Control.Applicative
+import           Control.Monad.Gen
+import           Control.Monad.Reader
+import qualified Data.Map             as M
 
-data IExpr = App IExpr CExpr
-           | Annot CExpr CExpr
-           | ETrue
-           | EFalse
-           | Bool
-           | Star
-           | Pi CExpr (CExpr -> CExpr)
-           | C String
-           | IGen Int
+data Expr = App Expr Expr
+          | Annot Expr Expr
+          | ETrue
+          | EFalse
+          | Bool
+          | Star
+          | Pi Expr (Expr -> Expr)
+          | Lam (Expr -> Expr)
+          | C String
+          | IGen Int
 
-data CExpr = Lam (CExpr -> CExpr)
-           | CI IExpr
 
-type NFI = IExpr
-type NFC = CExpr
+type NF = Expr
 
-inf :: IExpr -> NFI
-inf ETrue = ETrue
-inf EFalse = EFalse
-inf Bool = Bool
-inf Star = Star
-inf (C s) = C s
-inf (IGen i) = IGen i
-inf (Annot l r) = Annot (cnf l) (cnf r)
-inf (Pi t f) = Pi (cnf t) (cnf . f)
-inf (App l r) = case inf l of
-  (Annot (Lam f) (CI (Pi _ g))) -> Annot (cnf . f $ CI l) (cnf . g $ CI l)
-  l' -> App l' (cnf r)
+nf :: Expr -> NF
+nf ETrue = ETrue
+nf EFalse = EFalse
+nf Bool = Bool
+nf Star = Star
+nf (C s) = C s
+nf (IGen i) = IGen i
+nf (Annot l r) = nf l
+nf (Pi t f) = Pi (nf t) (nf . f)
+nf (App l r) = case nf l of
+  Lam f -> nf . f $ l
+  l' -> App l' (nf r)
 
-cnf :: CExpr -> NFC
-cnf (CI e) = CI (inf e)
-cnf (Lam f) = Lam (cnf . f)
+eqTerm ::NF ->NF -> Gen Expr Bool
+eqTerm Star Star = return True
+eqTerm Bool Bool = return True
+eqTerm ETrue ETrue = return True
+eqTerm EFalse EFalse = return True
+eqTerm (Annot l r) (Annot l' r') = (&&) <$> eqTerm l l' <*> eqTerm r r'
+eqTerm (App l r) (App l' r') = (&&) <$> eqTerm l l' <*> eqTerm r r'
+eqTerm (Pi t f) (Pi t' g) =
+  (&&) <$> eqTerm t t' <*> (gen >>= \v -> eqTerm (f v) (g v))
+eqTerm (IGen i) (IGen j) = return (i == j)
+eqTerm _ _ = return False
 
-ceqTerm :: NFC -> NFC -> Gen NFI Bool
-ceqTerm (Lam f) (Lam g) = gen >>= \v -> ceqTerm (f $ CI v) (g $ CI v)
-ceqTerm (CI l) (CI r) = ieqTerm l r
-ceqTerm _ _ = return False
-
-ieqTerm :: NFI -> NFI -> Gen IExpr Bool
-ieqTerm Star Star = return True
-ieqTerm Bool Bool = return True
-ieqTerm ETrue ETrue = return True
-ieqTerm EFalse EFalse = return True
-ieqTerm (Annot l r) (Annot l' r') = (&&) <$> ceqTerm l l' <*> ceqTerm r r'
-ieqTerm (App l r) (App l' r') = (&&) <$> ieqTerm l l' <*> ceqTerm r r'
-ieqTerm (Pi t f) (Pi t' g) =
-  (&&) <$> ceqTerm t t' <*> (gen >>= \v -> ceqTerm (f $ CI v) (g $ CI v))
-ieqTerm (IGen i) (IGen j) = return (i == j)
-ieqTerm _ _ = return False
-
-eqType :: NFC -> NFC -> Bool
-eqType l r = runGenWith (successor s) (IGen 0) $ ceqTerm l r
+eqType ::NF ->NF -> Bool
+eqType l r = runGenWith (successor s) (IGen 0) $ eqTerm l r
   where s (IGen i) = IGen (i + 1)
         s _ = error "Impossible!"
+
+data Env = Env { localVars :: M.Map Int NF
+               , constants :: M.Map String NF }
+type TyM = GenT Int (ReaderT Env Maybe)
+
+nferType :: Expr -> TyM NF
+nferType = undefined
+
+checkType :: Expr ->NF -> TyM ()
+checkType (Lam f) (Pi t g) = do
+  i <- gen
+  let t' = nf t
+      rTy = nf (g $ IGen i)
+  local (\e -> e{localVars = M.insert i t $ localVars e}) $
+    checkType (f $ IGen i) rTy
+checkType e t = nferType e >>= guard . eqType t
