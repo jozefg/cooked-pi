c8d5f777f381adb736c163f26cf0b0220d005a3d
Danny Gratzer <jozefg@cmu.edu>
2014-11-29 12:56:38 -0500

    Start of HOAS version

--- src/LambdaPi/HOAS.hs
+++ src/LambdaPi/HOAS.hs
@@ -1,1 +1,20 @@
 module LambdaPi.HOAS where
+import Control.Applicative
+import Control.Monad.Gen
+
+data IExpr = Var Int
+           | App IExpr CExpr
+           | Annot CExpr CExpr
+           | ETrue
+           | EFalse
+           | Bool
+           | Star
+           | Pi CExpr (CExpr -> CExpr)
+           | Const String
+           | IGen Int
+
+data CExpr = Lam (CExpr -> CExpr)
+           | CI IExpr
+
+type NFI = IExpr
+type NFC = CExpr
