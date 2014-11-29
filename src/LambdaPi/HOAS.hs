5043156d20e1fd32a41d500c22257da5db80361d
Danny Gratzer <jozefg@cmu.edu>
2014-11-29 12:57:03 -0500

    Ported eqTerm from Easy! to HOAS

--- src/LambdaPi/HOAS.hs
+++ src/LambdaPi/HOAS.hs
@@ -18,3 +18,23 @@ data CExpr = Lam (CExpr -> CExpr)
 
 type NFI = IExpr
 type NFC = CExpr
+
+-- *Whistle and fidget with hands*
+instance Enum IExpr where
+  toEnum = IGen
+  fromEnum _ = error "You're a bad person."
+
+ceqTerm :: NFC -> NFC -> Gen NFI Bool
+ceqTerm (Lam f) (Lam g) = gen >>= \v -> ceqTerm (f $ CI v) (g $ CI v)
+ceqTerm (CI l) (CI r) = ieqTerm l r
+ceqTerm _ _ = return False
+
+ieqTerm :: NFI -> NFI -> Gen IExpr Bool
+ieqTerm Star Star = return True
+ieqTerm Bool Bool = return True
+ieqTerm ETrue ETrue = return True
+ieqTerm EFalse EFalse = return True
+ieqTerm (Pi t f) (Pi t' g) =
+  (&&) <$> ceqTerm t t' <*> (gen >>= \v -> ceqTerm (f $ CI v) (g $ CI v))
+ieqTerm (IGen i) (IGen j) = return (i == j)
+ieqTerm _ _ = return False
