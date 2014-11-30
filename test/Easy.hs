915e9331497249d9965a84f91345c66449f6aa53
Danny Gratzer <jozefg@cmu.edu>
2014-11-30 01:05:35 -0500

    Added some simple tests for Easy

--- test/Easy.hs
+++ test/Easy.hs
@@ -1,5 +1,29 @@
 module Main where
+import LambdaPi.Easy
 import Test.Tasty
+import Test.Tasty.HUnit
+
+assertType :: String -> TestName -> (CExpr, CExpr) -> TestTree
+assertType s n (e, t) = testCase n $ assertBool s (hasType e t)
+
+constants :: TestTree
+constants = testGroup "Constant Tests"
+            [ assertType "ETrue is wrong" "True" (CI ETrue, CI Bool)
+            , assertType "EFalse is wrong" "False" (CI EFalse, CI Bool)
+            , assertType "Bool is wrong" "Bool" (CI Bool, CI Star) ]
+
+boolId :: TestTree
+boolId = assertType "Simple lambdas failed" "Bool identity"
+         (Lam (CI $ Var 0), CI $ Pi (CI Bool) (CI Bool))
+
+app :: TestTree
+app = assertType "Application fails" "Application"
+      (CI $
+       App (Annot (Lam . CI $ Var 0)
+            (CI $ Pi (CI Bool) (CI Bool)))
+           (CI ETrue)
+      , CI Bool)
 
 main :: IO ()
-main = defaultMain . testGroup "simple-easy Tests" $ []
+main = defaultMain . testGroup "simple-easy Tests"
+       $ [constants, boolId, app]
